a314da832a4a597405fd4f9a492665851cd1aaf6
Daniel Mascena <danielmascena@gmail.com>
2025-07-23 18:48:29 +0100

    Add solutions for LeetCode 1717 and 1948
    Added TypeScript implementations for LeetCode problems 1717 (Maximum Score From Removing Substrings) and 1948 (Delete Duplicate Folders in System), including test cases for problem 1717.

---
+++ 1948.delete-duplicate-folders-in-system.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1948 lang=typescript
+ *
+ * [1948] Delete Duplicate Folders in System
+ */
+
+// @lc code=start
+function deleteDuplicateFolder(paths: string[][]): string[][] {
+    
+};
+// @lc code=end
+
