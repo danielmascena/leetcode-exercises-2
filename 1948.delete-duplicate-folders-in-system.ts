3b2c1f5cc030660387da7206053d7d01ca2745ec
Daniel Mascena <danielmascena@gmail.com>
2025-07-27 00:27:58 +0100

    feat: Add initial solution for 1948

--- 1948.delete-duplicate-folders-in-system.ts
+++ 1948.delete-duplicate-folders-in-system.ts
@@ -1,3 +1,4 @@
+import { expect } from "bun:test";
 /*
  * @lc app=leetcode id=1948 lang=typescript
  *
@@ -6,7 +7,56 @@
 
 // @lc code=start
 function deleteDuplicateFolder(paths: string[][]): string[][] {
-    
-};
-// @lc code=end
+  const remove = new Set<string>();
+  const safe = new Set<string>();
+  const grp = new Map<string, string[][]>();
 
+  paths.forEach((path, idx) => {
+    if (path.length > 1) {
+      const last = path.at(-1)!;
+      const arr = grp.get(last) ?? [];
+      arr.push([...path].reverse());
+      grp.set(last, arr);
+    }
+  });
+  grp.forEach((arr) => {
+    if (arr.length > 1) {
+      arr.sort((a, b) => a.length - b.length);
+      const limit = arr?.[0]?.length ?? 0;
+      arr.forEach((a) => {
+        var ss = "";
+        a.forEach((c, i) => {
+          if (i < limit) {
+            ss = c + ss;
+          } else {
+            safe.add(c);
+          }
+        });
+        remove.add(ss);
+      });
+    }
+  });
+  safe.forEach((c) => grp.delete(c));
+  console.log(remove, "safe >>", safe, "@@@ ", grp);
+}
+// @lc code=end
+deleteDuplicateFolder([
+  ["a"],
+  ["c"],
+  ["d"],
+  ["a", "b"],
+  ["c", "b"],
+  ["d", "a"],
+]);
+deleteDuplicateFolder([
+  ["a"],
+  ["c"],
+  ["a", "b"],
+  ["c", "b"],
+  ["a", "b", "x"],
+  ["a", "b", "x", "y"],
+  ["w"],
+  ["w", "y"],
+]);
+deleteDuplicateFolder([["a", "b"], ["c", "d"], ["c"], ["a"]]);
+//expect(deleteDuplicateFolder([["a"],["c"],["d"],["a","b"],["c","b"],["d","a"]])?.map(v => v.join())).toContainAllValues(["d","d,a"]);
