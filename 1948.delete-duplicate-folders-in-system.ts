2ef4023a582513557369516fa6d115f132de4044
Daniel Mascena <danielmascena@gmail.com>
2025-07-28 10:37:17 +0100

    Refactor deleteDuplicateFolder and update tests
    Removed the previous implementation of deleteDuplicateFolder and replaced it with an empty function stub. Updated and cleaned up test cases, removing unused code and improving test clarity.

--- 1948.delete-duplicate-folders-in-system.ts
+++ 1948.delete-duplicate-folders-in-system.ts
@@ -6,57 +6,31 @@ import { expect } from "bun:test";
  */
 
 // @lc code=start
-function deleteDuplicateFolder(paths: string[][]): string[][] {
-  const remove = new Set<string>();
-  const safe = new Set<string>();
-  const grp = new Map<string, string[][]>();
-
-  paths.forEach((path, idx) => {
-    if (path.length > 1) {
-      const last = path.at(-1)!;
-      const arr = grp.get(last) ?? [];
-      arr.push([...path].reverse());
-      grp.set(last, arr);
-    }
-  });
-  grp.forEach((arr) => {
-    if (arr.length > 1) {
-      arr.sort((a, b) => a.length - b.length);
-      const limit = arr?.[0]?.length ?? 0;
-      arr.forEach((a) => {
-        var ss = "";
-        a.forEach((c, i) => {
-          if (i < limit) {
-            ss = c + ss;
-          } else {
-            safe.add(c);
-          }
-        });
-        remove.add(ss);
-      });
-    }
-  });
-  safe.forEach((c) => grp.delete(c));
-  console.log(remove, "safe >>", safe, "@@@ ", grp);
-}
+function deleteDuplicateFolder(paths: string[][]): string[][] {}
 // @lc code=end
-deleteDuplicateFolder([
-  ["a"],
-  ["c"],
-  ["d"],
-  ["a", "b"],
-  ["c", "b"],
-  ["d", "a"],
-]);
-deleteDuplicateFolder([
-  ["a"],
-  ["c"],
-  ["a", "b"],
-  ["c", "b"],
-  ["a", "b", "x"],
-  ["a", "b", "x", "y"],
-  ["w"],
-  ["w", "y"],
-]);
-deleteDuplicateFolder([["a", "b"], ["c", "d"], ["c"], ["a"]]);
-//expect(deleteDuplicateFolder([["a"],["c"],["d"],["a","b"],["c","b"],["d","a"]])?.map(v => v.join())).toContainAllValues(["d","d,a"]);
+
+const fm = (a: string[]) => a.join("");
+
+//expect(
+//  deleteDuplicateFolder([
+//    ["a"],
+//    ["c"],
+//    ["d"],
+//    ["a", "b"],
+//    ["c", "b"],
+//    ["d", "a"],
+//  ]).flatMap(fm)
+//).toContainAllValues([["d"], ["d", "a"]].flatMap(fm));
+
+expect(
+  deleteDuplicateFolder([
+    ["a"],
+    ["c"],
+    ["a", "b"],
+    ["c", "b"],
+    ["a", "b", "x"],
+    ["a", "b", "x", "y"],
+    ["w"],
+    ["w", "y"],
+  ]).flatMap(fm)
+).toContainAllValues([["c"], ["c", "b"], ["a"], ["a", "b"]].flatMap(fm));
