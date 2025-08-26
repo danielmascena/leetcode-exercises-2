e35eec422d863c3b0ccc348e4811de2f673651a4
Daniel Mascena <danielmascena@gmail.com>
2025-08-27 00:07:09 +0100

    Add solution for LeetCode 3000 in TypeScript
    Implements the areaOfMaxDiagonal function to find the area of the rectangle with the longest diagonal from a list of dimensions. Includes test cases for validation.

---
+++ 3000.maximum-area-of-longest-diagonal-rectangle.ts
@@ -0,0 +1,47 @@
+import { expect } from "bun:test";
+/*
+ * @lc app=leetcode id=3000 lang=typescript
+ *
+ * [3000] Maximum Area of Longest Diagonal Rectangle
+ */
+
+// @lc code=start
+function areaOfMaxDiagonal(dimensions: number[][]): number {
+  const { sqrt, max } = Math;
+  let diagLgth = 0;
+  let area = 0;
+  dimensions.forEach(([lgth = 0, wdth = 0]) => {
+    const dl = sqrt(lgth * lgth + wdth * wdth);
+
+    if (dl >= diagLgth) {
+      diagLgth = dl;
+      area = max(area, lgth * wdth);
+    }
+  });
+  return area;
+}
+// @lc code=end
+
+expect(
+  areaOfMaxDiagonal([
+    [9, 3],
+    [8, 6],
+  ])
+).toBe(48);
+expect(
+  areaOfMaxDiagonal([
+    [3, 4],
+    [4, 3],
+  ])
+).toBe(12);
+expect(
+  areaOfMaxDiagonal([
+    [6, 5],
+    [8, 6],
+    [2, 10],
+    [8, 1],
+    [9, 2],
+    [3, 5],
+    [3, 5],
+  ])
+).toBe(20);
