6600f64b769bf8e3edfc54b36e7895329be76645
Daniel Mascena <danielmascena@gmail.com>
2025-10-05 23:55:26 +0100

    new version with prefix

--- 11.container-with-most-water.ts
+++ 11.container-with-most-water.ts
@@ -7,29 +7,33 @@ import { expect } from "bun:test";
 
 // @lc code=start
 function maxArea(height: number[]): number {
-  const { max, min, abs } = Math;
-  const cpos = height
-    .map((v, i): [number, number] => [v, i])
-    .sort(([a, b], [c, d]) => a - c);
+  const { max, min } = Math;
+  const len = height.length;
+  const prefix: [number, number][] = [];
   let ans = 0;
+  height.forEach((v, i) => {
+    const [pv = 0, pi = 0] = prefix[i - 1] ?? [];
 
-  for (let idx = cpos.length - 1; idx > 0; idx--) {
-    const [v, i] = cpos[idx]!;
-    let t = ans;
-
-    for (let j = idx - 1; j >= 0; j--) {
-      const [v2, i2] = cpos[j]!;
-      const t2 = min(v, v2) * abs(i - i2);
-
-      t = max(t, t2);
+    if (v > pv && v - pv > i - pi) {
+      prefix[i] = [v, i];
+    } else {
+      prefix[i] = [pv, pi];
     }
-    ans = max(ans, t);
-  }
+  });
+  console.log(prefix);
+  height.forEach((v, i) => {
+    const [pv, pi] = prefix[i]!;
+    const dt = i - pi;
+    const av = min(v, pv);
+    ans = max(ans, dt * av);
+  });
   return ans;
 }
 // @lc code=end
 
 expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
 expect(maxArea([1, 1])).toBe(1);
+expect(maxArea([1, 2, 1])).toBe(2);
 expect(maxArea([2, 3, 4, 5, 18, 17, 6])).toBe(17);
+expect(maxArea([1, 2, 4, 3])).toBe(4);
 expect(maxArea([8, 10, 14, 0, 13, 10, 9, 9, 11, 11])).toBe(80);
