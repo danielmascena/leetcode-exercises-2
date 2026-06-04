355e8d5b0c83d95cd763dd583d2dd6eb5b4b1826
Daniel Mascena <danielmascena@gmail.com>
2026-06-04 12:39:17 +0100

    final ver

--- src/c/3633.earliest-finish-time-for-land-and-water-rides-i.c
+++ src/c/3633.earliest-finish-time-for-land-and-water-rides-i.c
@@ -18,76 +18,7 @@ void printArr(int *arr, const int size)
  */
 
 // @lc code=start
-#define min(a, b) (((a) < (b)) ? (a) : (b))
 
-#define max(a, b) (((a) > (b)) ? (a) : (b))
-
-int earliestFinishTime(int *landStartTime, int landStartTimeSize, int *landDuration, int landDurationSize, int *waterStartTime, int waterStartTimeSize, int *waterDuration, int waterDurationSize)
-{
-    int ans = INT_MAX;
-    int tmp = INT_MAX;
-    int end = 0;
-
-    for (int i = 0; i < waterDurationSize; i++)
-    {
-        const int dur = waterDuration[i] + waterStartTime[i];
-        end = max(end, dur);
-        tmp = min(tmp, waterStartTime[i]);
-    }
-    int *war = calloc(end + 1, sizeof(int));
-    int *warp = calloc(end + 1, sizeof(int));
-
-    for (int i = 0; i < waterDurationSize; i++)
-    {
-        war[waterStartTime[i]] = waterDuration[i];
-    }
-    for (int i = tmp, v = INT_MAX, z = INT_MAX; i <= end;)
-    {
-        if (war[i])
-        {
-            const int zv = i + war[i];
-            z = min(z, war[i]);
-            v = min(v, zv);
-
-            while (i <= zv)
-            {
-                warp[i] = z;
-                war[i] = v;
-                i++;
-            }
-        }
-        else
-        {
-            war[i] = v;
-            warp[i] = z;
-            i++;
-        }
-    }
-    printf("tmp %d\n", tmp);
-    printArr(war, end + 1);
-    printArr(warp, end + 1);
-
-    for (int i = 0; i < landDurationSize; i++)
-    {
-        const int lst = landStartTime[i];
-        const int ldu = landDuration[i];
-        const int tld = lst + ldu;
-        const int pws = war[lst];
-        const int lwd = warp[tld];
-
-        if (lst - pws >= 0 && pws)
-        {
-            ans = min(ans, tld);
-        }
-        if (lwd)
-            ans = min(ans, tld + lwd);
-        if (pws)
-            ans = min(ans, pws + ldu);
-    }
-    free(war);
-    free(warp);
-    return ans;
-}
 // @lc code=end
 
 int main(void)
