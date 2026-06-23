a1425eaccd6b742469a63fccaaf127863c42b4c0
Daniel Mascena <danielmascena@gmail.com>
2026-06-23 14:59:33 +0100

    freeing memory

--- src/c/3731.find-missing-elements.c
+++ src/c/3731.find-missing-elements.c
@@ -1,37 +1,42 @@
-#define max(a, b)(((a) > (b)) ? (a) : (b))
+#include <limits.h>
+#include <stdlib.h>
 
-#define min(a, b)(((a) < (b)) ? (a) : (b))
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
+#define min(a, b) (((a) < (b)) ? (a) : (b))
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* findMissingElements(int* nums, int numsSize, int* returnSize) {
-    int* arr;
-    int* ans;
-    int count = 0;
-    int higher = 0;
-    int lower = INT_MAX;
+int *findMissingElements(int *nums, int numsSize, int *returnSize) {
+  int *arr;
+  int *ans;
+  int count = 0;
+  int higher = 0;
+  int lower = INT_MAX;
 
-    for (int i = 0; i < numsSize; i++) {
-        const int num = nums[i];
-        higher = max(num, higher);
-        lower = min(num, lower);
-    }
-    int len = (higher - lower) + 1;
-    arr = calloc(len, sizeof(int));
+  for (int i = 0; i < numsSize; i++) {
+    const int num = nums[i];
+    higher = max(num, higher);
+    lower = min(num, lower);
+  }
+  int len = (higher - lower) + 1;
+  arr = calloc(len, sizeof(int));
 
-    for (int i = 0; i < numsSize; i++) {
-        arr[nums[i] - lower]++;
-    }
-    for (int i = 0; i < len; i++) {
-        if (!arr[i]) count++;
-    }
-    *returnSize = count;
-    ans = calloc(count, sizeof(int));
+  for (int i = 0; i < numsSize; i++) {
+    arr[nums[i] - lower]++;
+  }
+  for (int i = 0; i < len; i++) {
+    if (!arr[i])
+      count++;
+  }
+  *returnSize = count;
+  ans = calloc(count, sizeof(int));
 
-    for (int i = 0, j = 0; i < len && j < count; i++) {
-        if (!arr[i]) {
-            ans[j++] = i+lower;
-        }
+  for (int i = 0, j = 0; i < len && j < count; i++) {
+    if (!arr[i]) {
+      ans[j++] = i + lower;
     }
-    return ans;
+  }
+  free(arr);
+  return ans;
 }
